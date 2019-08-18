3592d1fe03c8902a059db4621d9ad81976046fcd
Gaurav Luthra <gaurav@Gauravs-MacBook-Pro.local>
2019-08-18 19:59:40 +0530

    Updates

---
+++ precache-manifest.b18a80040b8eb5b9beb30b3ca4ab4898.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246c112b8a05f41222af41fb30b543d9",
+    "url": "/luthraG/luthrag.github.io/index.html"
+  },
+  {
+    "revision": "d7eeafd1934990acf68d",
+    "url": "/luthraG/luthrag.github.io/static/css/main.9440cd6e.chunk.css"
+  },
+  {
+    "revision": "508154cf3cfec6caa467",
+    "url": "/luthraG/luthrag.github.io/static/js/2.f4142c0b.chunk.js"
+  },
+  {
+    "revision": "d7eeafd1934990acf68d",
+    "url": "/luthraG/luthrag.github.io/static/js/main.2d58a928.chunk.js"
+  },
+  {
+    "revision": "029db3f5e851482de8f4",
+    "url": "/luthraG/luthrag.github.io/static/js/runtime~main.e4c737ca.js"
+  },
+  {
+    "revision": "123e31da23ad9f984861e33f92699980",
+    "url": "/luthraG/luthrag.github.io/static/media/motofix.123e31da.jpeg"
+  },
+  {
+    "revision": "0a36edb446a48964cebd66ded11720b2",
+    "url": "/luthraG/luthrag.github.io/static/media/newgen.0a36edb4.png"
+  },
+  {
+    "revision": "a838cfe4267b28ce81ab24237bf0f50c",
+    "url": "/luthraG/luthrag.github.io/static/media/profiler.a838cfe4.jpeg"
+  }
+]);
